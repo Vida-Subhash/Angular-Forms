48b9c1ccbdd9ae12d776fb997094bf0522e5cec3
[vida-subhash] <[subhashshetty3773@gmail.com]>
2021-07-01 20:25:09 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  states = ['Andhra Pradesh', 'Assam', 'Bihar', 'Goa', 'Gujarat', 'Haryana', 'Karnataka','Kerala', '	Maharashtra',
+            'Meghalaya', 'Punjab', 'Rajasthan', 'Tamil Nadu' , 'Telangana', 'West Bengal', 'Uttar Pradesh'  ];
   title = 'Angular-Forms';
+
+  submit(myForm:any){
+    console.log(myForm);
+  }
+
+
 }
